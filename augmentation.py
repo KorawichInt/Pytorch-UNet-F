a7ae03e578243d31ef3737a0c39bba668f16bc60
KorawichInt <korawich.inthamusik@gmail.com>
2024-09-11 01:08:28 +0700

    modify augmentation only images

--- augmentation.py
+++ augmentation.py
@@ -19,20 +19,28 @@ def augmentation(input_path, filename, output_path):
     # Open the image file
     image_path = os.path.join(input_path, filename)
     image = Image.open(image_path)
-    ### Augmentation
-    # croped_img = v2.RandomCrop(size=(224, 224))(image)
-    blured_img = v2.GaussianBlur(kernel_size=9, sigma=(0.8, 1.2))(image)
-    brightnessed_img = v2.ColorJitter(brightness=(1.1,1.5))(image)
-    darknessed_img = v2.ColorJitter(brightness=(0.6,0.9))(image)
 
-    # Save the augmented images (if needed)
+    # Extract filename
     split_filename = filename.split('.')
     name = split_filename[0]
     surname = split_filename[1]
-    # croped_img.save(os.path.join(output_path, f'{name}_cropped.{surname}'))
-    blured_img.save(os.path.join(output_path, f'{name}_blurred.{surname}'))
-    brightnessed_img.save(os.path.join(output_path, f'{name}_brightnessed.{surname}'))
-    darknessed_img.save(os.path.join(output_path, f'{name}_darknessed.{surname}'))
+    
+    if surname == '.jpg':
+        ### Augmentation
+        # croped_img = v2.RandomCrop(size=(224, 224))(image)
+        blured_img = v2.GaussianBlur(kernel_size=9, sigma=(0.8, 1.2))(image)
+        brightnessed_img = v2.ColorJitter(brightness=(1.1,1.5))(image)
+        darknessed_img = v2.ColorJitter(brightness=(0.6,0.9))(image)
+
+        # croped_img.save(os.path.join(output_path, f'{name}_cropped.{surname}'))
+        blured_img.save(os.path.join(output_path, f'{name}_blurred.{surname}'))
+        brightnessed_img.save(os.path.join(output_path, f'{name}_brightnessed.{surname}'))
+        darknessed_img.save(os.path.join(output_path, f'{name}_darknessed.{surname}'))
+    else:
+        # image.save(os.path.join(output_path, f'{name}_cropped.{surname}'))
+        image.save(os.path.join(output_path, f'{name}_blurred.{surname}'))
+        image.save(os.path.join(output_path, f'{name}_brightnessed.{surname}'))
+        image.save(os.path.join(output_path, f'{name}_darknessed.{surname}'))
 
 
 # Loop through each file in the input folder
